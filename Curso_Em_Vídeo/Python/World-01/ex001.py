b2db52cbf963b32a1831f1f7cf23b875d8eee0ec
Max Braga <cbcc.max@gmail.com>
2021-02-08 16:42:17 -0300

    Adicionando programas do py
    primeiros comandos
    exercicio 01

---
+++ Curso_Em_Vídeo/Python/World-01/ex001.py
@@ -0,0 +1,6 @@
+#Exercicio 01
+# Entrar com o nome de uma pessoa, e retornar uma mensagem de boas vindas
+
+nome = input('Qual é o seu nome?')
+
+print('Olá ' + nome + '! Prazer em te conhecer!')
---
+++ Curso_Em_Vídeo/Python/World-01/first_commands.py
@@ -0,0 +1,29 @@
+
+# O conteudo do print para ser exposto na tela pode estar com 
+    # -> aspas simples
+    # -> aspas duplas 
+# escreve na tela a frase
+print ('Olá, Mundo!')  
+
+#operadores
+print (2 + 2)
+
+#concatenando com '+'
+print ('Olá ' + 'Mundo!')
+print ('2' + '2')
+
+
+# VARIAVEIS
+
+nome = 'Max'
+idade = 23
+peso = 100
+print (nome, idade, peso)
+ 
+#ATRIBUIR VARIAVEIS E MOSTRAR AS VARIÁVEIS
+
+nome2 = input('Qual é o seu nome?')
+idade2 = input('Qual é a sua idade?')
+peso2 = input('Qual é o seu peso?')
+print (nome2, idade2, peso2)
+ 
