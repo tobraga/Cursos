0160cc8cf1e263c181b8afd38c7fd7c3988d63f5
Max Braga <cbcc.max@gmail.com>
2021-02-08 18:51:21 -0300

    atualizando
    atualizando ex003
    entrandos com os arquivos da aula 06 - tipos primitivos e saida de dados

---
+++ Curso_Em_Vídeo/Python/World-01/TratandoDados_e_FazendoContas/testando_02.py
@@ -0,0 +1,21 @@
+
+n1 = int(input('Valor: '))
+n2 = int(input('Valor: '))
+
+s = n1 + n2
+print('Soma: {}'.format(s))
+print('--------------------------')
+print('Soma entre {} e {} = {}'.format(n1, n2, s))
+
+############################################################
+#n = int(input('Valor: '))
+#n = float(input('Valor: '))
+#n = bool(input('Valor: '))
+#n = str(input('Valor: '))
+#print(type(n))         - retorna o tipo da variavel
+#print(n.isnumeric())    - retorna se o valor é numerico
+#print(n.alpha())    - retorna se o valor é alfabetico
+#print(n.isalnum())    - retorna se o valor é alfanumerico
+#print(n.isupper())    - retorna se o valor está em letras maiusculas
+
+
